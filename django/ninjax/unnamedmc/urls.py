bb0db92a12545f3c7cbe709a3205e9a213272ef7
dragonstar373 <dragonstar373@gmail.com>
2024-02-18 07:45:17 +0000

    "save for the night"

--- django/ninjax/unnamedmc/urls.py
+++ django/ninjax/unnamedmc/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     #path("<directory after web address>", <view function in views.py>, name="<name of tab(?)>"),
-    path("", views.tut_home, name="home"),
+    path("", views.tut_home, name="As I walk through the valley of the shadow of death, I take a look at myself and realize there's nothing left"),
     path("test/", views.test, name="Test-page"),
+    path("home/", views.home, name="home"),
 
 ]
